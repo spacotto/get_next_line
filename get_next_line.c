081927ce61438d09bd1f474ea85a6720e52051d9
spacotto <silvia.pacotto@gmail.com>
2025-11-17 23:36:24 +0100

    Add get_next_line function implementation
    Implement the get_next_line function to read lines from a file descriptor, handling buffer and newline detection.

--- get_next_line.c
+++ get_next_line.c
@@ -12,5 +12,35 @@
 
 #include "get_next_line.h"
 
+char  *read_line()
+
 char	*get_next_line(int fd)
-{}
+{
+  char  *line;
+  static char buffer[BUFFER_SIZE] = {0};
+  ssize_t len;
+  char *found;
+
+  if (memchr(buffer, '\n', strlen(buffer))
+  {
+    handle_it;
+  }
+
+  while (1)
+    {
+     len = read(fd, buffer, BUFFER_SIZE);
+      buffer[len] = 0;
+      if (len < 0)
+        return (NULL);
+      if (memchr(buffer, '\n', len) || len == 0)
+        break; 
+    }
+  found = memchr(buffer, '\n', len);
+  if (found)
+  {
+    line = calloc(sizeof(char), len - strlen(found));
+    line = memmove(line, found, len - strlen(found));
+  }
+  buffer = buffer + (len - strlen(found));
+  return (line);
+}
