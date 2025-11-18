1160e3f0a75e7a19a37d0a6721f4e8b9daf1c56a
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2025-11-18 18:19:06 +0100

    general[ update

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,34 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 11:22:24 by spacotto          #+#    #+#             */
-/*   Updated: 2025/11/18 15:04:52 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/11/18 17:23:35 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char  *read_line()
+static int	find_line()
+{
+	
+}
+
+static int	make_line()
+{
+
+}
+
+static char	*read_line(int fd)
+{
+	int read_bytes = read(fd, buffer, BUFFER_SIZE);
+}
 
 char	*get_next_line(int fd)
 {
-  char  *line;
-  static char buffer[BUFFER_SIZE] = {0};
-  ssize_t len;
-  char *found;
-
-// you're in the filled buffer: look for \n
-
-// read into the buffer
-  return (line);
+	char  *line;
+	static t_buffer buffer = {0};
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = read_line();
+	return (line);
 }
