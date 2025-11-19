4c89e3ed10d87945555ce517aaf0e1e525d53e4a
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2025-11-19 17:14:40 +0100

    gnl update

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,29 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 11:22:24 by spacotto          #+#    #+#             */
-/*   Updated: 2025/11/18 17:23:35 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/11/19 17:13:28 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	find_line()
+// Given that gnl shall be called in while loops, it's better to start with the
+// assumption that our buffer has already been filled. In fact, when we use big
+// BUFFER_SIZE, we don't need to perform further readings, for we've gathered
+// all the necessary data.
+static int	find_line(t_buffer *buffer)
 {
 	
 }
 
-static int	make_line()
+// Once we find the end of the line (\n or \0), we can start composing the line
+// we will return.
+static int	make_line(t_buffer *buffer)
 {
 
 }
 
-static char	*read_line(int fd)
+static char	*read_line(int fd, t_buffer *buffer)
 {
 	int read_bytes = read(fd, buffer, BUFFER_SIZE);
 }
@@ -30,7 +36,7 @@ static char	*read_line(int fd)
 char	*get_next_line(int fd)
 {
 	char  *line;
-	static t_buffer buffer = {0};
+	static t_buffer buffer = {{0}, NULL}; // Declaring the whole structure as static requires the init of all its elements 
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
