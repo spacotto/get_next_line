3edd63fc6542914c331d8e34fe26a2aa542a6acd
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2025-11-21 12:48:03 +0100

    test update

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 11:22:14 by spacotto          #+#    #+#             */
-/*   Updated: 2025/11/20 13:04:11 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/11/21 12:27:30 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,8 +63,8 @@ void    *ft_memset(void *s, int c, size_t n);
 // COLORS
 // ============================================================================
 
-# define BOLD "\033[1m"
 # define RESET "\033[0m"
+
 # define GRAY "\033[0;90m"
 # define RED "\033[0;91m"
 # define GREEN "\033[0;92m"
@@ -73,5 +73,14 @@ void    *ft_memset(void *s, int c, size_t n);
 # define MAGENTA "\033[0;95m"
 # define CYAN "\033[0;96m"
 # define WHITE "\033[0;97m"
+
+# define BOLD_GRAY 		"\033[1;90m"
+# define BOLD_RED 		"\033[1;91m"
+# define BOLD_GREEN 	"\033[1;92m"
+# define BOLD_YELLOW	"\033[1;93m"
+# define BOLD_BLUE		"\033[1;94m"
+# define BOLD_MAGENTA 	"\033[1;95m"
+# define BOLD_CYAN 		"\033[1;96m"
+# define BOLD_WHITE 	"\033[1;97m"
 
 #endif
--- get_next_line_test_bonus.c
+++ get_next_line_test_bonus.c
@@ -6,70 +6,70 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 12:00:51 by spacotto          #+#    #+#             */
-/*   Updated: 2025/11/20 18:26:32 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/11/21 12:42:59 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
-#include <string.h>
 
-static void	open_files(int **fd, int ac, char **av)
+static int	close_files(int *fd, int open)
 {
 	int i = 0;
-	int f = 2;
-	while (f < ac)
-	{
-		fd[i] = open(av[f], O_RDONLY);
-		if (fd[i] < 0)
-		{
-			printf(BOLD RED "Invalid source file!" RESET);
-			close_files(fd, i);
-			return (-1);
-		}
-		i++;
-		f++;
-	}
-}
-
-static void	close_files(int **fd, int open)
-{
-	int i = 0;
-	while (fd[i] <= open)
+	while (i <= open)
 	{
 		close(fd[i]);
 		i++;
 	}
+	return (0);
 }
 
+static int	open_files(int *fd, int ac, char **av)
+{
+	int i = 0;
+	while (i < ac - 2)
+	{
+		fd[i] = open(av[i + 2], O_RDONLY);
+		if (fd[i] < 0)
+		{
+			printf(BOLD_RED "ERROR: Invalid source file(s)!" RESET);
+			close_files(fd, i);
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
 	int fd[FD_MAX] = {0};
-	int lines = 0;
-	int files = 2;
-	
+	int l = 0;	// The line we're getting 
+	int f = 0;	// The file from which we're getting the line
+
 	if (ac <= 2 || atoi(av[1]) == 0)
 	{
-		printf(BOLD RED "ERROR: Invalid format!\n" RESET);
-		printf(BOLD YELLOW "Valid format: [1] Number of lines [2] Source file(s)\n" RESET);
+		printf(BOLD_RED "ERROR: Invalid format!\n" RESET);
+		printf(YELLOW "Valid format: [1] Number of lines [2] Source file(s)\n" RESET);
 	}
 	if (ac > 2)
 	{
-		open_files(fd, ac, av);
-		int i = 0;
-		while (lines < atoi(av[1]))
+		if (open_files(fd, ac, av) == -1)
+			return (-1);
+		while (l < atoi(av[1]))
 		{
-			while (files < ac)
+			f = 0;
+			printf(BOLD_CYAN "\nGET NEXT LINE!\n" RESET);
+			while (f < ac - 2)
 			{
-				char *line = get_next_line(fd[i]);
-				printf(YELLOW "File [%d] Line [%d]\n" RESET, fd[i], lines + 1);
+				char *line = get_next_line(fd[f]);
+				printf(YELLOW "File [%d] Line [%d]\n" RESET, fd[f], l);
 				printf("%s", line);
 				free(line);
-				i++;
+				f++;
 			}
-			lines++;
+			l++;
 		}
-		close(fd, ac - 2);
+		close_files(fd, ac - 2);
 	}
 	return (0);
 }
