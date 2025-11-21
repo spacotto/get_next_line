9434b8e8085d2269a0bdcc805b08bac4a1df161e
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2025-11-21 16:22:02 +0100

    update .h gnl

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -57,15 +57,24 @@ void    *ft_memset(void *s, int c, size_t n);
 // COLORS
 // ============================================================================
 
-# define BOLD "\033[1m"
-# define RESET "\033[0m"
-# define GRAY "\033[0;90m"
-# define RED "\033[0;91m"
-# define GREEN "\033[0;92m"
-# define YELLOW "\033[0;93m"
-# define BLUE "\033[0;94m"
-# define MAGENTA "\033[0;95m"
-# define CYAN "\033[0;96m"
-# define WHITE "\033[0;97m"
+# define RESET 			"\033[0m"
+
+# define GRAY 			"\033[0;90m"
+# define RED 			"\033[0;91m"
+# define GREEN 			"\033[0;92m"
+# define YELLOW 		"\033[0;93m"
+# define BLUE 			"\033[0;94m"
+# define MAGENTA		"\033[0;95m"
+# define CYAN 			"\033[0;96m"
+# define WHITE 			"\033[0;97m"
+
+# define BOLD_GRAY 		"\033[1;90m"
+# define BOLD_RED 		"\033[1;91m"
+# define BOLD_GREEN 	"\033[1;92m"
+# define BOLD_YELLOW	"\033[1;93m"
+# define BOLD_BLUE		"\033[1;94m"
+# define BOLD_MAGENTA 	"\033[1;95m"
+# define BOLD_CYAN 		"\033[1;96m"
+# define BOLD_WHITE 	"\033[1;97m"
 
 #endif
