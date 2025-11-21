c9f8f38916ee5846d9debc96af60b219e126ca93
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2025-11-21 16:30:36 +0100

    update test gnl

--- gnl/get_next_line_test.c
+++ gnl/get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 15:40:18 by spacotto          #+#    #+#             */
-/*   Updated: 2025/11/20 12:01:32 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/11/21 16:29:37 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,28 @@
 
 int	main(int ac, char **av)
 {
-	int		fd;
-	int		i;
-	char	*line;
-
-	i = 0;
-	if (ac < 2)
+	if (ac < 2 || atoi(av[1]) == 0)
 	{
-		printf(BOLD RED "ERROR: Invalid format!\n" RESET);
-		printf(BOLD YELLOW "Valid format: [1] Number of lines [2] Source file\n" RESET);
+		printf(BOLD_RED "ERROR: Invalid format!\n" RESET);
+		printf(YELLOW "Valid format: [1] Number of lines [2] Source file\n" RESET);
 	}
 	if (ac > 2)
 	{
-		fd = open(av[2], O_RDONLY);
+		int fd = open(av[2], O_RDONLY);
 		if (fd < 0)
-			printf(BOLD RED "Invalid source file! " RESET);
+			printf(BOLD_RED "Invalid source file! " RESET);
 		else
 		{
+			int i = 0;
 			while (i < atoi(av[1]))
 			{
-				line = get_next_line(fd);
+				char *line = get_next_line(fd);
 				printf("%s", line);
-				i++;
-				free(line);				
+				free(line);
+				i++;				
 			}
-			close(fd);
 		}
+		close(fd);
 	}
 	return (0);
 }
