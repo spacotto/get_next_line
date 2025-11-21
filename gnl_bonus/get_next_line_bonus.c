1a6e417369db94bac77c2b55ac67cf9841a14593
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2025-11-21 18:24:16 +0100

    bonus update

--- gnl_bonus/get_next_line_bonus.c
+++ gnl_bonus/get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 11:22:03 by spacotto          #+#    #+#             */
-/*   Updated: 2025/11/21 17:30:22 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/11/21 18:23:48 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 static int	find_line(t_buffer *sbuffer)
 {
+	sbuffer->start = sbuffer->bookmark;
 	if (sbuffer->start != NULL)
 	{
 		sbuffer->bookmark = ft_memchr(sbuffer->start, '\n', BUFFER_SIZE);
-		sbuffer->start = sbuffer->bookmark;
 		return (1);
 	}
 	else
@@ -26,14 +26,14 @@ static int	find_line(t_buffer *sbuffer)
 
 static void	make_line(t_buffer *sbuffer, t_line *sline)
 {
-	char	*new;
-	size_t	old_len;
+	char	*chunk;
+	size_t	chunk_len;
 
-	old_len = sline->line_len;
+	chunk_len = sline->line_len;
 	if (!sbuffer->start)
 		return ;
 	sline->line_len += sline->copy - sbuffer->start + 1;
-	new = ft_calloc(sline->line_len + 1, sizeof(char));
+	chunk = ft_calloc(sline->line_len + 1, sizeof(char));
 	if (!new)
 	{
 		free(sline->line);
@@ -41,9 +41,9 @@ static void	make_line(t_buffer *sbuffer, t_line *sline)
 		return ;
 	}
 	if (*sline->line)
-		ft_memcpy(new, sline->line, old_len);
-	ft_memcpy(new + old_len, sbuffer->start, sline->line_len - old_len);
-	new[sline->line_len] = '\0';
+		ft_memcpy(chunk, sline->line, chunk_len);
+	ft_memcpy(chunk + chunk_len, sbuffer->start, sline->line_len - chunk_len);
+	chunk[sline->line_len] = '\0';
 	sbuffer->start = sline->copy + 1;
 	if (sbuffer->start > sbuffer->end)
 		sbuffer->start = NULL;
@@ -60,15 +60,12 @@ static void	read_line(int fd, t_buffer *sbuffer, t_line *sline)
 	{
 		bytes_read = read(fd, sbuffer->buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
-			{	
-				sbuffer->start = NULL; 
-				free(sline->line);
-				sline->line = NULL;
-			}
+		{
+			sline->line = NULL;
+			return ;
+		}
 		sbuffer->start = sbuffer->buffer;
 		sbuffer->end = sbuffer->start + bytes_read - 1;
-		if (sbuffer->end < sbuffer->start)
-			sbuffer->start = NULL;
 		if (bytes_read < BUFFER_SIZE)
 			break ;
 	}
