b8aba8b7cafa876235ece7c11b8406556092340c
spacotto <silvia.pacotto@gmail.com>
2025-11-23 12:36:06 +0100

    Update get_next_line_utils.c

--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -74,3 +74,13 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
