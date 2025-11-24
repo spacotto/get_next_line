e2fd3e4d72c7e80ff6daa4ddaa6d69bcd3f8f233
Silvia Pacotto <spacotto@c1r1p2.42lehavre.fr>
2025-11-24 16:22:40 +0100

    test main gnl update

--- gnl/get_next_line_test.c
+++ gnl/get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 15:40:18 by spacotto          #+#    #+#             */
-/*   Updated: 2025/11/21 16:29:37 by spacotto         ###   ########.fr       */
+/*   Updated: 2025/11/24 16:13:09 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,42 @@
 
 int	main(int ac, char **av)
 {
-	if (ac < 2 || atoi(av[1]) == 0)
+	if (ac < 3)
 	{
-		printf(BOLD_RED "ERROR: Invalid format!\n" RESET);
+		printf(BOLD_RED "ERROR: not enough arguments!\n" RESET);
 		printf(YELLOW "Valid format: [1] Number of lines [2] Source file\n" RESET);
+		return (-1);
 	}
-	if (ac > 2)
+	if (ac > 3)
 	{
+		printf(BOLD_RED "ERROR: too many arguments!\n" RESET);
+		printf(YELLOW "Valid format: [1] Number of lines [2] Source file\n" RESET);
+		return (-1);
+	}
+	if (ac == 3)
+	{
+		if (atoi(av[1]) == 0)
+		{
+			printf(BOLD_RED "Error: invalid number of lines!\n" RESET);
+			printf(YELLOW "Valid format: [1] Number of lines [2] Source file\n" RESET);
+			return (-1);
+		}
+		printf("LOL");
 		int fd = open(av[2], O_RDONLY);
 		if (fd < 0)
-			printf(BOLD_RED "Invalid source file! " RESET);
-		else
 		{
-			int i = 0;
-			while (i < atoi(av[1]))
-			{
-				char *line = get_next_line(fd);
-				printf("%s", line);
-				free(line);
-				i++;				
-			}
+			printf(BOLD_RED "Error: invalid source file!\n" RESET);
+			printf(YELLOW "Valid format: [1] Number of lines [2] Source file\n" RESET);
+			close(fd);
+			return (-1);
+		}
+		int i = 0;
+		while (i < atoi(av[1]))
+		{
+			char *line = get_next_line(fd);
+			printf("%s", line);
+			free(line);
+			i++;				
 		}
 		close(fd);
 	}
